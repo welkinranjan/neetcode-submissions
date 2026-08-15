b3b462f2c956fbf8a80fb52bd4808cdb2ea217ab
welkinranjan <68800964+welkinranjan@users.noreply.github.com>
2026-08-14 20:37:40 -0700

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.cpp
@@ -0,0 +1,33 @@
+class TimeMap {
+public:
+    using pis=pair<int,string>;
+    unordered_map<string,vector<pis>> umap;
+
+    TimeMap() {
+        umap.clear();
+    }
+    
+    void set(string key, string value, int timestamp) {
+        umap[key].push_back({timestamp,value});
+    }
+    
+    string get(string key, int timestamp) {
+        if(umap.find(key)==umap.end())
+            return "";
+        vector<pis>& v=umap[key];
+        int n=v.size();
+        int low=0,high=n-1;
+        string ans="";
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int val=v[mid].first;
+            if(val<=timestamp){
+                ans=v[mid].second;
+                low=mid+1;
+            }
+            else
+                high=mid-1;
+        }
+        return ans;
+    }
+};
